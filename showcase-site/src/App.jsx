44480ce0bea5c9e0f32acc3bfcd68631fde6d681
SSIEK <ssiek@student.unimelb.edu.au>
2025-03-20 15:41:20 +1100

    added cross site embedding, trying to figure out flex sizing

--- showcase-site/src/App.jsx
+++ showcase-site/src/App.jsx
@@ -4,6 +4,25 @@ import Dropdown from './Dropdown'
 import DropZone from './Dropzone'
 
 function App() {
+
+  let bgUrl = "https://gamepress.gg";
+  if (bgUrl){
+    return (
+      <div className="App">
+        <div className="min-h-screen bg-white-900">
+          <Navbar />
+          <Dropdown/>
+          <object data={bgUrl} style={{flex:1}}>
+            
+            Error: embedded data could not be loaded.
+          </object>
+          <DropZone/>
+          
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <div className="min-h-screen bg-white-900">
