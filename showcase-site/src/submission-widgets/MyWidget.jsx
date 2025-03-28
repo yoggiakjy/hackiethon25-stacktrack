c49e51602a2e930e94116cbcf9d1859ad465e19b
Antony Kim <antonykim7@gmail.com>
2025-03-29 08:51:27 +1100

    Feature fix for all entry types.

--- showcase-site/src/submission-widgets/MyWidget.jsx
+++ showcase-site/src/submission-widgets/MyWidget.jsx
@@ -6,22 +6,33 @@ import cryptoIcon from "./assets/crypto.png";
 import stockIcon from "./assets/stock.png";
 import editIcon from "./assets/edit.png";
 import trashIcon from "./assets/trash.png";
-import crossIcon from './assets/cross.png';
+import crossIcon from "./assets/cross.png";
 
 const DEFAULT_INVESTMENTS = [
   {
     id: 1,
+    entry: "VOO",
     date: new Date(2025, 2, 14),
     type: "Stock",
-    description: "NVIDIA",
+    description: "ETF Investment",
     amount: "500",
   },
   {
-    id:2, 
-    date: new Date(2025, 2, 14),
+    id: 2,
+    entry: "BTC",
+    date: new Date(2025, 2, 16),
     type: "Crypto",
-    description: "BITCOIN",
+    description: "Bitcoin Investment",
     amount: "1500",
+  },
+  {
+    id: 3,
+    entry: "SAVINGS",
+    date: new Date(2025, 3, 11),
+    type: "Savings",
+    description: "Wage from job",
+    amount: "11000",
+    rate: "5",
   },
 ];
 
@@ -45,10 +56,13 @@ const MyWidget = () => {
     setInvestments(investments.filter((investment) => investment.id !== id));
   };
 
-  const toggleEditMode = () => setIsEditMode(!isEditMode)
+  const toggleEditMode = () => setIsEditMode(!isEditMode);
 
   useEffect(() => {
-    const newTotal = investments.reduce((sum, investment) => sum + Number(investment.amount), 0);
+    const newTotal = investments.reduce(
+      (sum, investment) => sum + Number(investment.amount),
+      0
+    );
     setNetWorth(newTotal.toFixed(2));
   }, [investments]);
 
@@ -57,7 +71,15 @@ const MyWidget = () => {
       {/* Main networth view */}
       <div className="flex flex-col justify-center items-center w-full bg-gradient-to-tr from-black via-gray-900 to-blue-900 shadow-2xl border-gray-900 border-[0.001px] rounded-xl p-6">
         <p className="text-sm font-extralight text-gray-300">Your Stack</p>
-        <p className={`font-medium text-center text-neutral-100 mt-1 ${netWorth.length > 12 ? "text-2xl" : "text-5xl"}`}>{netWorth.length < 12 ? `$${netWorth}` : 'I am awed by your riches...'}</p>
+        <p
+          className={`font-medium text-center text-neutral-100 mt-1 ${
+            netWorth.length > 12 ? "text-2xl" : "text-5xl"
+          }`}
+        >
+          {netWorth.length < 12
+            ? `$${netWorth}`
+            : "I am awed by your riches..."}
+        </p>
 
         {/* Change buttons */}
         <div className="flex justify-center items-center w-full gap-10 text-neutral-50 mt-8">
@@ -146,48 +168,24 @@ const MyWidget = () => {
           {investments.map((investment) => (
             <div
               key={investment.id}
-              className="flex justify-between items-center w-full text-white gap-5"
+              className="flex justify-between items-center w-full text-white"
             >
-              <div className="flex gap-5">
-                {investment.type === "Savings" && (
-                  <div className="bg-gradient-to-tr from-black via-gray-900 to-green-900 rounded-xl p-1">
-                    <img
-                      src={bankIcon}
-                      alt="Bank icon"
-                      height={512}
-                      width={512}
-                      className="w-[1rem]"
-                    />
-                  </div>
-                )}
-                {investment.type === "Crypto" && (
-                  <div className="bg-gradient-to-tr from-black via-gray-900 to-purple-900 rounded-xl p-1">
-                    <img
-                      src={cryptoIcon}
-                      alt="Crypto Icon"
-                      width={512}
-                      height={512}
-                      className="w-[2rem]"
-                    />
-                  </div>
-                )}
-                {investment.type === "Stock" && (
-                  <div className="bg-gradient-to-tr from-black via-gray-900 to-orange-900 rounded-xl p-1">
-                    <img
-                      src={stockIcon}
-                      alt="Stock Icon"
-                      width={512}
-                      height={512}
-                      className="w-[2rem]"
-                    />
-                  </div>
-                )}
-                <p>{investment.description}</p>
-              </div>
-              <div className="flex justify-center items-center gap-4">
-                <p>{`$${investment.amount}`}</p>
+              {investment.type === "Savings" && (
+                <SavingsEntry investment={investment} />
+              )}
+              {investment.type === "Crypto" && (
+                <CryptoEntry investment={investment} />
+              )}
+              {investment.type === "Stock" && (
+                <StockEntry investment={investment} />
+              )}
+
+              <div className="flex justify-center items-center">
                 {isEditMode && (
-                  <button onClick={() => handleDeleteInvestment(investment.id)} className="cursor-pointer">
+                  <button
+                    onClick={() => handleDeleteInvestment(investment.id)}
+                    className="cursor-pointer ml-3"
+                  >
                     <img
                       src={trashIcon}
                       alt="Delete icon"
@@ -201,6 +199,112 @@ const MyWidget = () => {
             </div>
           ))}
         </div>
+      </div>
+    </div>
+  );
+};
+
+const SavingsEntry = ({ investment }) => {
+  return (
+    <div className="flex justify-between items-center w-full text-white ">
+      <div className="flex justify-start items-center w-full gap-5">
+        <div className="flex gap-5">
+          <div className="bg-gradient-to-tr from-black via-gray-900 to-green-900 rounded-xl p-1">
+            <img
+              src={bankIcon}
+              alt="Bank icon"
+              height={512}
+              width={512}
+              className="w-[2rem]"
+            />
+          </div>
+        </div>
+
+        <div className="w-[4.5rem] flex flex-col justify-center items-start">
+          <p className="text-xs font-light text-stone-400">
+            {investment.date.toLocaleDateString()}
+          </p>
+          <p className="">{investment.entry}</p>
+        </div>
+
+        <p className="max-w-[50%] border-l border-white pl-3 text-sm font-light overflow-hidden">
+          {investment.description}
+        </p>
+      </div>
+
+      <div className="flex flex-col justify-start items-center">
+        <p className="text-xs font-light text-stone-400">{`${investment.rate}% p.a`}</p>
+        <p>{`$${investment.amount}`}</p>
+      </div>
+    </div>
+  );
+};
+
+const CryptoEntry = ({ investment }) => {
+  return (
+    <div className="flex justify-between items-center w-full text-white ">
+      <div className="flex justify-start items-center w-full gap-5">
+        <div className="flex gap-5">
+          <div className="bg-gradient-to-tr from-black via-gray-900 to-purple-900 rounded-xl p-1">
+            <img
+              src={cryptoIcon}
+              alt="Crypto Icon"
+              width={512}
+              height={512}
+              className="w-[2rem]"
+            />
+          </div>
+        </div>
+
+        <div className="w-[4.5rem] flex flex-col justify-center items-start">
+          <p className="text-xs font-light text-stone-400">
+            {investment.date.toLocaleDateString()}
+          </p>
+          <p className="">{investment.entry}</p>
+        </div>
+
+        <p className="max-w-[50%] border-l border-white pl-3 text-sm font-light overflow-hidden">
+          {investment.description}
+        </p>
+      </div>
+
+      <div className="flex flex-col justify-start items-center">
+        <p>{`$${investment.amount}`}</p>
+      </div>
+    </div>
+  );
+};
+
+const StockEntry = ({ investment }) => {
+  return (
+    <div className="flex justify-between items-center w-full text-white ">
+      <div className="flex justify-start items-center w-full gap-5">
+        <div className="flex gap-5">
+          <div className="bg-gradient-to-tr from-black via-gray-900 to-orange-900 rounded-xl p-1">
+            <img
+              src={stockIcon}
+              alt="Stock Icon"
+              width={512}
+              height={512}
+              className="w-[2rem]"
+            />
+          </div>
+        </div>
+
+        <div className="w-[4.5rem] flex flex-col justify-center items-start">
+          <p className="text-xs font-light text-stone-400">
+            {investment.date.toLocaleDateString()}
+          </p>
+          <p className="">{investment.entry}</p>
+        </div>
+
+        <p className="max-w-[50%] border-l border-white pl-3 text-sm font-light overflow-hidden">
+          {investment.description}
+        </p>
+      </div>
+
+      <div className="flex flex-col justify-start items-center">
+        <p>{`$${investment.amount}`}</p>
       </div>
     </div>
   );
